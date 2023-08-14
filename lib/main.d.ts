a567cf618d0f99fbad501d459b8188e384592f7a
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2023-08-14 13:29:59 -0600

    1.0.0

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,8 +1,6 @@
 export { makeRange, getRuntime, exec, List } from './util.d.ts';
 export type { Base10, Base2 } from './util.d.ts';
 export { ValidType } from './validation.d.ts';
-export { InvalidTokenError, skip } from './tokenize.fn.d.ts';
+export { InvalidTokenError, skip, default as tokenize } from './tokenize.fn.d.ts';
 export type { Token, TokenOptions } from './tokenize.fn.d.ts';
-
-import tokenize from './tokenize.fn.d.ts';
-export {tokenize}
+export { DenoSymbol, NodeSymbol, default as Inspectable } from './Inspectable.class.ts';
